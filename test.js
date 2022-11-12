1264e3bc1695e5615d390a93c5ff0f338ae5d69c
Erasmus <erasmusmensah00@gmail.com>
2022-11-12 20:27:52 +0000

    update: minor

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
-// @flow
+// // @flow
 
-import * as React from 'react';
+// import * as React from 'react';
 
-type Props = {
-  foo: number,
-  bar?: string,
-};
+// type Props = {
+//   foo: number,
+//   bar?: string,
+// };
 
-function MyComponent(props: Props) {
-  props.doesNotExist; // Error! You did not define a `doesNotExist` prop.
+// function MyComponent(props: Props) {
+//   props.doesNotExist; // Error! You did not define a `doesNotExist` prop.
 
-  return <div>{props.bar}</div>;
-}
+//   return <div>{props.bar}</div>;
+// }
 
-<MyComponent foo={42} />
+// <MyComponent foo={42} />
