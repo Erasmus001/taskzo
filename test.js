684189988fab4ffae5886cdccb2c7276ce801b01
Erasmus <erasmusmensah00@gmail.com>
2022-11-13 01:40:59 +0000

    initialiazed commit: First commit for Taskzo project...

---
+++ test.js
@@ -0,0 +1,16 @@
+// @flow
+
+import * as React from 'react';
+
+type Props = {
+  foo: number,
+  bar?: string,
+};
+
+function MyComponent(props: Props) {
+  props.doesNotExist; // Error! You did not define a `doesNotExist` prop.
+
+  return <div>{props.bar}</div>;
+}
+
+<MyComponent foo={42} />
