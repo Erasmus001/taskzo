75a2c850ca051add2bdab1f893880787a6f04fc5
Erasmus <erasmusmensah00@gmail.com>
2022-11-22 08:35:08 +0000

    minor change

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,12 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import styles from './Dashboard.module.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
-import Board from '../../Components/Board/Board'
 
 const Dashboard = () => {
   return (
     <div className={styles.dashboard}>
       <Sidebar />
-      <Board />
     </div>
   )
 }
